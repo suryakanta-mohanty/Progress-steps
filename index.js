7d9266c7d5d235e11ed8daa2ff84870620f1dd50
suryakanta-mohanty <suryakantamohanty855@gmail.com>
2024-10-18 15:02:51 +0530

    making fully funcational to next button

--- index.js
+++ index.js
@@ -3,17 +3,44 @@ const nextBtn = document.querySelector('.js-next-btn');
 const steps = document.querySelectorAll('.steps');
 const progressBar = document.querySelector('.progress_bar');
 
-const steps_length = steps.length - 1;
-// console.log(steps_length);
-
-const progress_bar_full = 100;
-let progress_bar_final;
+let currentActive = 1;
 
 nextBtn.addEventListener('click', ()=>{
-  progress_bar_final = `${progress_bar_full}`/`${steps_length}`;
-  // console.log(progress_bar_final);
-  progressBar.style.width = `${progress_bar_final}%`;
+  currentActive ++;
+  
+  if(currentActive > steps.length){
+    currentActive = steps.length;
+  }
 
-  steps[1].classList.add('active');
-  prevBtn.removeAttribute('disabled');
+  updateStep();
 });
+
+prevBtn.addEventListener('click', ()=>{
+
+});
+
+function updateStep(){
+  steps.forEach((step, index) =>{
+    if(index < currentActive){
+      step.classList.add('active');
+
+    }else{
+      step.classList.remove('active');
+    }
+  });
+
+  const actives = document.querySelectorAll('.active');
+  
+  progressBar.style.width = (actives.length - 1) / (steps.length - 1) * 100 + '%';
+
+  if(currentActive === 1){
+    prevBtn.disabled = true;
+
+  }else if(currentActive === steps.length){
+    nextBtn.disabled = true;
+
+  }else{
+    prevBtn.disabled = false;
+    nextBtn.disabled = false;
+  }
+}
